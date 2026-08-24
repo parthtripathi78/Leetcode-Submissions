c1d31854b44fac63853d97e6111ba935cb269f7c
Parth Tripathi <premiumgaming34@gmail.com>
2026-08-24 16:37:48 +0530

    Time: 87 ms (6.57%), Space: 27.6 MB (7.43%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -0,0 +1,30 @@
+class Solution(object): 
+ 
+    def minFallingPathSum(self, matrix): 
+ 
+        """ 
+        :type matrix: List[List[int]] 
+        :rtype: int 
+        """ 
+ 
+        n = len(matrix) 
+        memo = {} 
+
+        def s(i, j): 
+            if j < 0 or j >= n: 
+                return float('inf') 
+
+            if (i, j) in memo: 
+                return memo[(i, j)] 
+ 
+            if i == n - 1: 
+                return matrix[i][j] 
+ 
+            LD = matrix[i][j] + s(i + 1, j - 1) 
+            D = matrix[i][j] + s(i + 1, j) 
+            RD = matrix[i][j] + s(i + 1, j + 1) 
+
+            memo[(i, j)] = min(LD, D, RD) 
+            return memo[(i, j)]
+
+        return min(s(0, j) for j in range(n))
