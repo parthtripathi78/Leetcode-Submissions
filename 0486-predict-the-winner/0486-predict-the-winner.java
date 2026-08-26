5dd946e2bc2c7c03d8da1d3e03fd7de4442514cf
Parth Tripathi <premiumgaming34@gmail.com>
2026-08-26 12:10:45 +0530

    Time: 75 ms (7.14%), Space: 42.8 MB (54.52%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,19 +1,40 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
+
         int n = nums.length;
-        int[][] dp = new int[n][n];
+        int total = 0;
 
-        for (int i = 0; i < n; i++)
-            dp[i][i] = nums[i];
-
-        for (int d = 1; d < n; d++) {
-            for (int i = 0; i + d < n; i++) {
-                int j = i + d;
-                dp[i][j] = Math.max(nums[i] - dp[i + 1][j],
-                                    nums[j] - dp[i][j - 1]);
-            }
+        for (int num : nums) {
+            total += num;
         }
 
-        return dp[0][n - 1] >= 0;
+        int p1 = helper(nums, 0, n - 1);
+
+        int p2 = total - p1;
+
+        return p1 >= p2;
+    }
+
+    int helper(int[] nums, int i, int j) {
+
+        if (i > j) {
+            return 0;
+        }
+
+        if (i == j) {
+            return nums[i];
+        }
+
+        int take_i = nums[i] + Math.min(
+            helper(nums, i + 2, j),
+            helper(nums, i + 1, j - 1)
+        );
+
+        int take_j = nums[j] + Math.min(
+            helper(nums, i + 1, j - 1),
+            helper(nums, i, j - 2)
+        );
+
+        return Math.max(take_i, take_j);
     }
 }
